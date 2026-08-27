7f4fbb08f25a79afd26253fe04d4dd37d59d579f
Matthew Justin Intoy <2402438@cclab.usls.edu>
2026-08-27 10:24:03 +0800

    Done activity.

--- HELLO_WORLD/app/src/main/java/com/example/hello_world/MainActivity.java
+++ HELLO_WORLD/app/src/main/java/com/example/hello_world/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.hello_world;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        final Button toClick = findViewById(R.id.button2);
+        final TextView myText = findViewById(R.id.myTextView);
+        final TextView toSet = findViewById(R.id.editTexText);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        toClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toGet = toSet.getText().toString();
+                myText.setText(toGet);
+            }
         });
     }
 }
