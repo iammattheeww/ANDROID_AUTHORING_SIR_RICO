dd72c186709fa15077abfd1919eafc27426be122
Matthew Justin Intoy <2402438@cclab.usls.edu>
2026-09-08 10:26:32 +0800

    Bottom Navigation.

---
+++ CONSTRAINT_TEST/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        BottomNavigationView myBottom = findViewById(R.id.myBottom);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v,insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.bottom, systemBars.bottom);
+            return insets;
+        });
+
+       myBottom.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+              int id = menuItem.getItemId();
+              if(id==R.id.dashboard_id){
+                  Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+              } else {
+                  Toast.makeText(MainActivity.this, "Welcome to Carts!", Toast.LENGTH_SHORT).show();
+              }
+
+               if(id==R.id.cart_id){
+                   Toast.makeText(MainActivity.this, "Welcome to Carts!", Toast.LENGTH_SHORT).show();
+               } else {
+                   Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+               }
+
+               return true;
+           }
+       });
+    }
+}
